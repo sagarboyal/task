265208d8d1dbc98c4f9b737c04d9e1e0cbf142f9
sagarboyal <sboyal12@gmail.com>
2026-04-20 13:35:45 +0530

    add and configure postgres database

--- src/main/java/com/main/task/TaskApplication.java
+++ src/main/java/com/main/task/TaskApplication.java
@@ -3,10 +3,13 @@ package com.main.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TaskApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(TaskApplication.class, args);
 	}
 
